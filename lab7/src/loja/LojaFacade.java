084e64624368ce997ffac8e98be0db29ca64ef05
Luiz-FS <luiz.silva@ccc.ufcg.edu.br>
2016-03-31 22:11:58 -0300

    Ajeitando identação

--- lab7/src/loja/LojaFacade.java
+++ lab7/src/loja/LojaFacade.java
@@ -9,48 +9,48 @@ import exceptions.SteamException;
 import exceptions.StringException;
 
 public class LojaFacade {
-	
+
 	private LojaController controladorLoja;
-	
+
 	public LojaFacade(){
 		controladorLoja = new LojaController();
 	}
 
 	public boolean addUsuario(String nome, String login, ExperienciaUsuario experiencia) {
-		
+
 		try {
 			return controladorLoja.addUsuario(nome, login, experiencia);
-			
+
 		} catch (SteamException e) {
 			return false;
 		}
 	}
 
 	public boolean addDinheioUsuario(String login, double quantDinheiro) {
-		
+
 		try {
 			return controladorLoja.addDinheioUsuario(login, quantDinheiro);
-			
+
 		} catch (SteamException e) {
 			return false;
 		}
 	}
 
 	public boolean containUsuario(String loginUsuario) {
-		
+
 		try {
 			return controladorLoja.containUsuario(loginUsuario);
-			
+
 		} catch (StringException e) {
 			return false;
 		}
 	}
 
 	public boolean registraJogada(String login, String nomeJogo, int score, boolean zerou) {
-		
+
 		try {
 			return controladorLoja.registraJogada(login, nomeJogo, score, zerou);
-			
+
 		} catch (SteamException e) {
 			return false;
 		}
@@ -58,24 +58,24 @@ public class LojaFacade {
 
 	public boolean venderJogo(String login, String nomeJogo, double preco, TipoDeJogo tipoJogo,
 			List<Jogabilidade> jogabilidades) {
-		
+
 		try {
 			return controladorLoja.venderJogo(login, nomeJogo, preco, tipoJogo, jogabilidades);
-			
+
 		} catch (SteamException e) {
 			return false;
 		}
 	}
 
 	public boolean usuarioContainsJogo(String loginUsuario, String nomeJogo) throws SteamException {
-		
+
 		try {
 			return controladorLoja.usuarioContainsJogo(loginUsuario, nomeJogo);
-			
+
 		} catch (SteamException e) {
 			return false;
 		}
 	}
-	
-	
+
+
 }
